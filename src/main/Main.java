b40948df53f8ee425d5c233976575a94137c55df
gneliana <gneliana@gmail.com>
2026-04-24 22:29:09 -0500

    add Hello World entry point

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Design Token Engine
+ * SE-350 Object Oriented Software Development
+ * Entry point: src/main/Main.java
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Design Token Engine - Hello World");
+        System.out.println("A tool for managing and exporting design tokens.");
+    }
+}
